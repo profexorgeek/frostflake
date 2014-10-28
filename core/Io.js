47603122bf27a9b31aa35f1fd237b69c2c2be246
Justin Johnson <justin.d.johnson@gmail.com>
2014-10-28 15:59:43 -0600

    Stripped ugly JSON Parse/Stringify hacks. Validated with JSLint

--- core/Io.js
+++ core/Io.js
@@ -4,18 +4,21 @@
     Provides methods for loading objects from JSON or converting them to JSON
 
     TODOS:
-    - Provide methods for interfacing with REST/JSON APIs
+    - JSON.Stringify and Parse fallbacks?
+    - Figure out how this fits in to the grander scheme of loading and saving entire levels
 
 ================================================================================================*/
 
+/*global console, Image, jQuery*/
 var frostFlake = (function (ff, $) {
-	
-	// loads json from a url
+    "use strict";
+
+    // loads json from a url
     ff.loadJson = function (url, successCallback, failCallback, alwaysCallback) {
         if (!successCallback) {
             successCallback = function (json) {
-                console.log("Loaded json from: " + url);
-            }
+                console.log("Loaded json from: " + url + " (" + json + ")");
+            };
         }
 
         if (!failCallback) {
@@ -26,39 +29,16 @@ var frostFlake = (function (ff, $) {
         $.getJSON(url).done(successCallback).fail(failCallback).always(alwaysCallback);
     };
 
-    // converts an object to a JSON string
-    ff.toJson = JSON.stringify || function (obj) {
-        console.log("Warning: JSON.stringify not found. Using fallback.");
-        var t = typeof (obj);
-        if (t != "object" || obj === null) {
-            if (t == "string") obj = '"' + obj + '"';
-            return String(obj);
-        }
-        else {
-            var n, v, json = [], arr = (obj && obj.constructor == Array);
-            for (n in obj) {
-                v = obj[n];
-                t = typeof(v);
-                if (t == "string") v = '"' + v + '"';
-                else if (t == "object" && v !== null) v = JSON.stringify(v);
-                json.push((arr ? "" : '"' + n + '":') + String(v));
-            }
-            return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
-        }
-    };
+    // converts an object to a JSON string. Stubbed to use fallback library.
+    ff.toJson = JSON.stringify;
 
-    // parses a json string, WARNING: uses eval if JSON object is not available
-    ff.fromJson = JSON.parse || function (str) {
-        console.log("Warning: JSON.parse not found. Using fallback.");
-        if (str === "") str = '""';
-        eval("var p=" + str + ";");
-        return p;
-    };
+    // parses a json string. Stubbed to eventually use fallback library.
+    ff.fromJson = JSON.parse;
 
     // loads an image from a URL
     ff.loadImage = function (url, loadedCallback) {
-        var path = url;
-        var img;
+        var path = url,
+            img;
 
         if (url.indexOf("data:") < 0 && url.indexOf("http") < 0) {
             path = ff.constants.CONTENT_DIRECTORY + "/" + url;
@@ -75,13 +55,12 @@ var frostFlake = (function (ff, $) {
 
         // execute callbacks on load
         img.onload = function () {
-            for (var i = 0; i < img.loadEvents.length; i += 1) {
+            var i;
+            for (i = 0; i < img.loadEvents.length; i += 1) {
                 img.loadEvents[i]();
             }
         };
-
         img.src = path;
-
         return img;
     };
 
