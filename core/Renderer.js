abddb526a8a09d8b2f98d985afd43bad0d06ef84
Justin Johnson <justin.d.johnson@gmail.com>
2014-10-30 16:12:01 -0600

    Major overhaul of the renderer. Now draws nested sprites recursively, maintaining the parent translation through the tree.

--- core/Renderer.js
+++ core/Renderer.js
@@ -1,160 +1,95 @@
 /* ===============================================================================================
 
     RENDERER.JS
-    Responsible for rendering a collection of sprites to a specified context (canvas)
-    using a camera to defined the projection.
+    The renderer is capable of taking a list of sprites, potentially with nested children,
+    and rendering them to a provided context using a camera projection.
+
+    The renderer is stateless, requiring a list of sprites, a camera and a context on every
+    draw call. This enables a single renderer to draw to multiple contexts with multiple
+    cameras.
 
 ================================================================================================*/
 
-var frostFlake = (function (ff, $) {
+var frostFlake = (function (ff) {
+    "use strict";
 
-    // TODO: this should not be a Class. Move this into a JavaScript native object type
-    ff.Renderer = Class.extend({
-        init:function(canvas, camera, background) {
-            this.canvas = canvas;
-            this.context = this.canvas.getContext("2d");
-            this.camera = (camera) ? camera : new ff.Camera();
-            this.background = (background) ? background : ff.constants.DEFAULT_BACKGROUND;
-            this.viewBounds = {
-                top: 0,
-                right: 0,
-                bottom: 0,
-                left: 0,
-                width: 0,
-                height: 0
-            };
+    ff.Renderer = ff.Class.extend({
+        // no constructor, renderer is stateless
 
-            this.boundsFirstUpdate = false;
-        },
+        // draws a list of sprites to a context using the provided camera's projection
+        // passing arguments allows renderer to be used for multiple different cameras and surfaces
+        draw: function (spriteList, camera, context, backgroundColor) {
+            var canvas = context.canvas,                                                        // canvas to draw to
+                cameraTranslationX = ff.math.invert(camera.position.x) + (canvas.width / 2),    // camera projection offset
+                cameraTranslationY = camera.position.y + (canvas.height / 2),                   // camera projection offset
+                fillColor = ff.defaultIfNoValue(backgroundColor, "rgb(0, 0, 0, 0)"),            // background or transparent
+                i;                                                                              // iterator, JSLint prefers here
 
-        setCamera:function(camera) {
-            this.camera = camera;
-        },
+            // fill canvas
+            context.fillStyle = fillColor;
+            context.fillRect(0, 0, canvas.width, canvas.height);
 
-        getCamera:function() {
-            return this.camera;
-        },
+            // perform camera projection translation
+            context.save();
+            context.translate(cameraTranslationX, cameraTranslationY);
 
-        setCanvas:function(canvas) {
-            if(!canvas) {
-                throw "Attempted to set invalid canvas on renderer."
-            }
-            else {
-                this.canvas = canvas;
-                this.context = canvas.getContext("2d");
-            }
-        },
-
-        getCanvas:function() {
-            return this.canvas;
-        },
-
-        updateViewBounds:function () {
-            this.viewBounds.top = this.camera.position.y + this.canvas.clientHeight / 2;
-            this.viewBounds.left = this.camera.position.x - this.canvas.clientWidth / 2;
-            this.viewBounds.bottom = this.viewBounds.top - this.canvas.clientHeight;
-            this.viewBounds.right = this.viewBounds.left + this.canvas.clientWidth;
-            this.viewBounds.width = this.viewBounds.right - this.viewBounds.left;
-            this.viewBounds.height = this.viewBounds.top - this.viewBounds.bottom;
-
-            this.canvas.width = this.viewBounds.width;
-            this.canvas.height = this.viewBounds.height;
-
-            this.boundsFirstUpdate = true;
-        },
-
-        draw:function(drawableList) {
-            if(!this.context) {
-                throw "Cannot draw: renderer has no valid context.";
+            // draw sprites, recurses on children
+            for (i = 0; i < spriteList.length; i += 1) {
+                this.drawSprite(spriteList[i], context);
             }
 
-            if(this.viewBounds.width != this.canvas.clientWidth || this.viewBounds.height != this.canvas.clientHeight) {
-                this.updateViewBounds();
+            // restore context
+            context.restore();
+        },
+
+        // draws sprite and all children recursively
+        drawSprite: function (sprite, context) {
+            var spriteTranslationX = sprite.position.x,
+                spriteTranslationY = ff.math.invert(sprite.position.y),
+                spriteRotation = sprite.rotation,
+                spriteAlpha = sprite.alpha,
+                srcWidth = sprite.textureCoordinates.right - sprite.textureCoordinates.left,
+                srcHeight = sprite.textureCoordinates.top - sprite.textureCoordinates.bottom,
+                i;
+
+            // apply transformation
+            context.save();
+            context.translate(spriteTranslationX, spriteTranslationY);
+            context.rotate(spriteRotation);
+
+            // set alpha
+            context.globalAlpha = spriteAlpha;
+
+            // draw sprite if texture is valid
+            if (ff.hasValue(sprite.texture)) {
+                context.drawImage(
+                    sprite.texture,
+                    sprite.textureCoordinates.left,
+                    sprite.textureCoordinates.top,
+                    srcWidth,
+                    srcHeight,
+                    sprite.width / -2,
+                    sprite.height / -2,
+                    sprite.width,
+                    sprite.height
+                );
             }
 
-            var xTarget;
-            var yTarget;
-            this.context.fillStyle = this.background;
-            this.context.fillRect(0,0, this.viewBounds.width, this.viewBounds.height);
-            this.camera.start(this.context);
-            for(var i = 0; i < drawableList.length; i++) {
-                var currentDrawable = drawableList[i];
-                xTarget = currentDrawable.position.x;
-                yTarget = ff.math.invert(currentDrawable.position.y);
-                this.context.save();
-                this.context.translate(xTarget, yTarget);
-                this.context.rotate(currentDrawable.rotation);
-                this.context.globalAlpha = currentDrawable.alpha;
-                if(currentDrawable instanceof ff.Sprite && currentDrawable.active) {
-                    this.drawSprite(currentDrawable);
+            // TODO: draw sprite radius if sprite.showRadius
+
+            // reset alpha
+            context.globalAlpha = 1;
+
+            // recurse through children
+            if (ff.hasValue(sprite.children)) {
+                for (i = 0; i < sprite.children.length; i += 1) {
+                    this.drawSprite(sprite.children[i], context);
                 }
-                else if(currentDrawable instanceof ff.Polygon && currentDrawable.active) {
-                    this.drawPolygon(currentDrawable);
-                }
-                this.context.restore();
-            }
-            this.camera.end(this.context);
-        },
-
-        drawPolygon:function(polygon) {
-            if (polygon.points.length < 2) {
-                return;
-            }
-            this.context.beginPath();
-            this.context.moveTo(polygon.points[0].x, polygon.points[0].y);
-            for (var p = 1; p < polygon.points.length; p++) {
-                var point = polygon.points[p];
-                this.context.lineTo(point.x, point.y);
             }
 
-            if (polygon.isClosed) {
-                this.context.lineTo(polygon.points[0].x, polygon.points[0].y);
-
-                // draw to one extra point to prevent end gaps in thick strokes
-                this.context.lineTo(polygon.points[1].x, polygon.points[1].y);
-            }
-
-            if (polygon.isFilled) {
-                this.context.fillStyle = polygon.fillColor;
-                this.context.fill();
-            }
-
-            this.context.lineWidth = polygon.lineWidth;
-            this.context.strokeStyle = polygon.lineColor;
-            this.context.stroke();
-        },
-
-        drawSprite:function(sprite) {
-            if(sprite.img === undefined || sprite.img === null) {
-                return;
-            }
-
-
-            if(!sprite.textureCoordinates) {
-                sprite.updateDimensions();
-            }
-
-            var srcWidth = sprite.textureCoordinates.right - sprite.textureCoordinates.left;
-            var srcHeight = sprite.textureCoordinates.bottom - sprite.textureCoordinates.top;
-
-            this.context.drawImage(
-                sprite.img,
-                sprite.textureCoordinates.left,
-                sprite.textureCoordinates.top,
-                srcWidth,
-                srcHeight,
-                sprite.width / -2,
-                sprite.height / -2,
-                sprite.width,
-                sprite.height);
-
-            if (sprite.radiusVisible) {
-                this.context.beginPath();
-                this.context.arc(0, 0, sprite.radius, 0, ff.math.twoPi, false);
-                this.context.strokeStyle = "#F00";
-                this.context.stroke();
-            }
+            // restore context
+            context.restore();
         }
     });
     return ff;
-}(frostFlake || {}, jQuery));
+}(frostFlake || {}));
