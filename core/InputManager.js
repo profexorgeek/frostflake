0c0e10c1948a7dc3d0401d898e45e3f89d3c625c
Justin Johnson <justin.d.johnson@gmail.com>
2014-11-02 17:56:39 -0700

    Implemented touch detection. InputManager now simulates mouse input on touch.

--- core/InputManager.js
+++ core/InputManager.js
@@ -13,12 +13,57 @@ var frostFlake = (function (ff, $) {
         init: function (canvas) {
             var mouse = ff.input.mouse,               // local reference to the mouse object
                 keyboard = ff.input.keyboard,         // local reference to the keyboard object
-                camera = ff.game.camera;              // local reference to the game camera
+                camera = ff.game.camera,              // local reference to the game camera
+                me = this;                            // self reference for events
 
             if (!ff.hasValue(canvas)) {
                 throw "InputManager needs a valid canvas to listen for input.";
             }
 
+            //============================================
+            // TOUCH EVENTS
+            //============================================
+            // simulate mouse movement on touch
+            $(canvas).on('touchmove', function (e) {
+                var touch = e.originalEvent.touches[0];
+                if(ff.hasValue(touch)) {
+                    me.updateMouseLocation(touch.clientX, touch.clientY);
+                }
+
+                if(mouse.inFrame === true) {
+                    e.preventDefault();
+                }
+            });
+
+            // simulate mousedown on touch
+            $(document).on('touchstart', function (e) {
+                var touch = e.originalEvent.touches[0];
+                if(ff.hasValue(touch)) {
+                    me.updateMouseLocation(touch.clientX, touch.clientY);
+                }
+                mouse.buttonsDown["Left"] = true;
+
+                if(mouse.inFrame === true) {
+                    e.preventDefault();
+                }
+            });
+
+            // simulate mouseup on touch
+            $(document).on('touchend', function (e) {
+                var touch = e.originalEvent.touches[0];
+                if(ff.hasValue(touch)) {
+                    me.updateMouseLocation(touch.clientX, touch.clientY);
+                }
+                mouse.buttonsDown["Left"] = false;
+
+                if(mouse.inFrame === true) {
+                    e.preventDefault();
+                }
+            });
+
+            //============================================
+            // MOUSE EVENTS
+            //============================================
             // handle mouse over canvas
             $(canvas).mouseenter(function () {
                 mouse.inFrame = true;
@@ -26,31 +71,39 @@ var frostFlake = (function (ff, $) {
             $(canvas).mouseleave(function () {
                 mouse.inFrame = false;
             });
+            
+            // handle mouse movement
+            $(canvas).on('mousemove', function (e) {
+                me.updateMouseLocation(e.offsetX, e.offsetY);
 
-            // update mouse position
-            $(canvas).mousemove(function (e) {
-                mouse.lastX = mouse.x;
-                mouse.lastY = mouse.y;
-                mouse.x = e.offsetX - (camera.viewPort.width / 2);
-                mouse.y = ff.math.invert(e.offsetY) + (camera.viewPort.height / 2);
-                mouse.changeX = mouse.x - mouse.lastX;
-                mouse.changeY = mouse.y - mouse.lastY;
-                mouse.worldX = camera.position.x + mouse.x;
-                mouse.worldY = camera.position.y + mouse.y;
+                if(mouse.inFrame === true) {
+                    e.preventDefault();
+                }
             });
 
-            // handle mouse button pressed
-            $(document).mousedown(function (e) {
+            // handle mouse button pressed, simulate presses on touch
+            $(document).on('mousedown', function (e) {
                 var buttonName = mouse.buttonCodes[e.which];
                 mouse.buttonsDown[buttonName] = true;
+
+                if(mouse.inFrame === true) {
+                    e.preventDefault();
+                }
             });
 
             // handle mouse button released
-            $(document).mouseup(function (e) {
+            $(document).on('mouseup', function (e) {
                 var buttonName = mouse.buttonCodes[e.which];
                 mouse.buttonsDown[buttonName] = false;
+
+                if(mouse.inFrame === true) {
+                    e.preventDefault();
+                }
             });
 
+            //============================================
+            // KEYBOARD EVENTS
+            //============================================
             // handle keyboard button press
             $(document).keydown(function (e) {
                 var keyName = keyboard.chars[e.which];
@@ -68,6 +121,21 @@ var frostFlake = (function (ff, $) {
                     e.preventDefault();
                 }
             });
+        },
+
+        // updates the mouse object based on the position where an event occurred
+        updateMouseLocation: function (locationX, locationY) {
+            var mouse = ff.input.mouse,
+                camera = ff.game.camera;
+
+            mouse.lastX = mouse.x;
+            mouse.lastY = mouse.y;
+            mouse.x = locationX - (camera.viewPort.width / 2);
+            mouse.y = ff.math.invert(locationY) + (camera.viewPort.height / 2);
+            mouse.changeX = mouse.x - mouse.lastX;
+            mouse.changeY = mouse.y - mouse.lastY;
+            mouse.worldX = camera.position.x + mouse.x;
+            mouse.worldY = camera.position.y + mouse.y;
         },
 
         // update mouse world coordinates from camera
