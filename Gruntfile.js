d3846ea49f471a01edb21e11fdf0fbf1f2b89803
Justin Johnson <justin.d.johnson@gmail.com>
2014-11-18 11:48:54 -0700

    Added "uglify" task to grunt. Now have build system to concat and minify frostflake. Moved files out of stupid "core" folder. Created copy of example that uses minified frostFlake for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,24 +7,36 @@ module.exports = function(grunt) {
             },
             dist: {
                 src: [
-                    "core/Class.js",
-                    "core/Animation.js",
-                    "core/Camera.js",
-                    "core/Input.js",
-                    "core/InputManager.js",
-                    "core/Io.js",
-                    "core/Renderer.js",
-                    "core/Sprite.js",
-                    "core/Util.js",
-                    "core/View.js",
+                    "Class.js",
+                    "Animation.js",
+                    "Camera.js",
+                    "Input.js",
+                    "InputManager.js",
+                    "Io.js",
+                    "Renderer.js",
+                    "Sprite.js",
+                    "Util.js",
+                    "View.js",
                     "frostFlake.js"
                 ],
                 dest: "dist/<%= pkg.name %>_<%= pkg.version %>_concat.js"
+            }
+        },
+        uglify: {
+            options: {
+                banner: "/*! <%= pkg.name %> Version: <%= pkg.version %> */\n"
+            },
+            dist : {
+                files: {
+                    "dist/<%= pkg.name %>_<%= pkg.version %>_min.js" : ["dist/<%= pkg.name %>_<%= pkg.version %>_concat.js"]
+                }
             }
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
     grunt.registerTask("combine", ["concat"]);
+    grunt.registerTask("package", ["concat", "uglify"]);
 }
