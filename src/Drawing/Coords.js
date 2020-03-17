33ae1068ca7ecf4ccbd6f3ce3dc5ed7230132700
Justin Johnson <justin.d.johnson@gmail.com>
2020-03-17 17:11:22 -0600

    Improved coords and added a spritesheet sprite

--- src/Drawing/Coords.js
+++ src/Drawing/Coords.js
@@ -3,4 +3,11 @@ class Coords {
     left = 0;
     width = 0;
     height = 0;
+
+    constructor(left = 0, top = 0, width = 0, height = 0) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+    }
 }
