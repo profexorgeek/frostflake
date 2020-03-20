ec2fdef523a8546bbaa4a4e2acbfd3c25a5edfe3
Justin Johnson <justin.d.johnson@gmail.com>
2020-03-20 15:33:08 -0600

    Renamed coords to frame in anticipation of animation

--- src/Drawing/Frame.js
+++ src/Drawing/Frame.js
@@ -1,13 +1,15 @@
-class Coords {
+class Frame {
     top = 0;
     left = 0;
     width = 0;
     height = 0;
+    seconds = 0;
 
-    constructor(left = 0, top = 0, width = 0, height = 0) {
+    constructor(left = 0, top = 0, width = 0, height = 0, seconds = 0) {
         this.left = left;
         this.top = top;
         this.width = width;
         this.height = height;
+        this.seconds = seconds;
     }
 }
