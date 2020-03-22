4d2b1f9bbd32ffc66a17e747b906fdfe481e2103
Jason Hammock <jason@arrayschool.com>
2020-03-22 08:08:35 -0600

    Added JS build script to be run from NPM

---
+++ scripts/build.js
@@ -0,0 +1,53 @@
+const concat = require('concat')
+    uglify = require('uglify-es'),
+    babel = require('@babel/core'),
+    fs = require('fs'),
+    path = require('path');
+
+const config = {
+    buildDirPath: path.dirname(__dirname) + '/build',
+    buildFileName: 'FrostFlake.min.js',
+    babelOptions: {
+        plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-private-methods"
+        ]
+    },
+    uglifyOptions: {
+        sourceMap: {
+            url: "inline"
+        }
+    }
+}
+
+concat([
+    './src/GameTime.js',
+    './src/Utility/MathUtil.js',
+    './src/Entities/Sprite.js',
+    './src/Entities/Camera.js',
+    './src/Drawing/CanvasRenderer.js',
+    './src/Drawing/Frame.js',
+    './src/Drawing/Animation.js',
+    './src/Views/View.js',
+    './src/Logging/Log.js',
+    './src/Input/Cursor.js',
+    './src/Input/Keys.js',
+    './src/Input/Mouse.js',
+    './src/Input/Input.js',
+    './src/FrostFlake.js'
+])
+.then(result => {
+    const transpiled = babel.transform(result, config.babelOptions);
+    const uglified = uglify.minify(transpiled.code, config.uglifyOptions);
+    cleanBuildDir(config.buildDirPath);
+    fs.writeFileSync(`${config.buildDirPath}/${config.buildFileName}`, uglified.code);
+}).catch(error => console.log(error));
+
+const cleanBuildDir = (buildDirPath) => {
+    if (fs.existsSync(buildDirPath)) {
+        fs.readdirSync(buildDirPath).forEach(filename => fs.unlinkSync(`${buildDirPath}/${filename}`));
+    } 
+    else {
+        fs.mkdirSync(buildDirPath);
+    }
+}
