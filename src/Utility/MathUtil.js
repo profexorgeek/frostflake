969d2dc05ab87ab34caf71d8fd3f1323196d1481
Justin Johnson <justin.d.johnson@gmail.com>
2020-03-25 17:22:48 -0600

    Added new vector methods to math util

--- src/Utility/MathUtil.js
+++ src/Utility/MathUtil.js
@@ -31,9 +31,46 @@ class MathUtil {
     }
 
     static vectorSubtract(v1, v2) {
-        let dx = v1.x - v2.x;
-        let dy = v1.y - v2.y;
-        return {x: dx, y: dy};
+        if(typeof v2 === "number") {
+            return {
+                x: v1.x - v2,
+                y: v1.y - v2
+            };
+        }
+        else {
+            return {
+                x: v1.x - v2.x,
+                y: v1.y - v2.y
+            };
+        }
+    }
+
+    static vectorMultiply(v1, v2) {
+        if(typeof v2 === "number") {
+            return {
+                x: v1.x * v2,
+                y: v1.y * v2
+            };
+        }
+        else {
+            return {
+                x: v1.x * v2.x,
+                y: v1.y * v2.y
+            };
+        }
+    }
+
+    static vectorNormalize(v1) {
+        let len = MathUtil.vectorLength(v1);
+        return len == 0 ? v1 : {x: (v1.x / len), y: (v1.y / len)};
+    }
+
+    static vectorDot(v1, v2) {
+        return (v1.x * v2.x) + (v1.y * v2.y);
+    }
+
+    static vectorLength(v1) {
+        return MathUtil.length(v1.x, v1.y);
     }
 
     static length(x, y) {
