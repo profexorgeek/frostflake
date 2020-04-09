ef95fff3093540f5331eb83b94ab641443e7d789
Justin Johnson <justin.d.johnson@gmail.com>
2020-04-09 10:55:47 -0600

    Added camera reset method

--- src/Positionables/Camera.js
+++ src/Positionables/Camera.js
@@ -5,4 +5,13 @@ class Camera extends Rectangle{
     constructor(width, height) {
         super(width, height);
     }
+
+    reset() {
+        this.resolution = 1;
+        this.antialias = false;
+        this.position = {x: 0, y: 0, rotation: 0}
+        this.velocity = {x: 0, y: 0, rotation: 0}
+        this.acceleration = {x: 0, y: 0, rotation: 0}
+        this.drag = 0;
+    }
 }
