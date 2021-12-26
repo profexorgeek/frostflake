3c66724f0cfa8cb667aee098303936a1f7c76dc1
Justin Johnson <justin.d.johnson@gmail.com>
2021-12-26 16:55:19 -0700

    Added tagging to Text class

--- src/Positionables/Text.ts
+++ src/Positionables/Text.ts
@@ -1,19 +1,18 @@
 import Positionable from "./Positionable";
 
 export default class Text extends Positionable {
-    font = '24px sans-serif'
-    content = 'Hello world';
-    fillStyle = 'white';
-    strokeStyle = 'black';
-    strokeWeight = 1;
-    textAlign = 'start';
-    textBaseline = 'alphabetic';
-    textDirection = 'ltf';
-    ignoreRotation = false;
+    font: string            = '24px sans-serif'
+    content: string         = 'Hello world';
+    fillStyle: string       = 'white';
+    strokeStyle: string     = 'black';
+    strokeWeight: number    = 1;
+    textAlign: string       = 'start';
+    textBaseline: string    = 'alphabetic';
+    textDirection: string   = 'ltf';
+    ignoreRotation: boolean = false;
 
-    constructor(str) {
+    constructor(str: string) {
         super();
-
         this.content = str;
     }
 }
