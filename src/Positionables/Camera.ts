37dba5eff5c784aeb271c5a84cff4363f3467b57
Justin Johnson <justin.d.johnson@gmail.com>
2021-12-28 14:58:30 -0700

    Camera now has typings
    https://github.com/profexorgeek/frostflake/issues/24

--- src/Positionables/Camera.ts
+++ src/Positionables/Camera.ts
@@ -1,36 +1,40 @@
 import MathUtil from '../Utility/MathUtil';
+import Position from './Position';
 import Rectangle from './Rectangle';
 
 export default class Camera extends Rectangle{
-    resolution = 1;
-    antialias = false;
-    lastPosition = {x: 0, y: 0, rotation: 0}
+    resolution: number      = 1;
+    antialias: boolean      = false;
+    lastPosition: Position  = new Position();
+    background: string      = "rgba(0,0,0,1)";
 
-    get randomPositionInView() {
-        return {
-            x: MathUtil.randomInRange(this.width / -2, this.width / 2),
-            y: MathUtil.randomInRange(this.height / -2, this.height / 2),
-            rotation: 0
-        };
+    get randomPositionInView(): Position {
+        return new Position(
+            MathUtil.randomInRange(this.width / -2, this.width / 2),
+            MathUtil.randomInRange(this.height / -2, this.height / 2),
+            0
+        );
     }
 
     constructor(width, height) {
         super(width, height);
     }
 
-    update() {
+    update(): void {
+
         this.lastPosition.x = this.position.x;
         this.lastPosition.y = this.position.y;
         this.lastPosition.rotation = this.position.rotation;
+
         super.update();
     }
 
-    reset() {
+    reset(): void {
         this.resolution = 1;
         this.antialias = false;
-        this.position = {x: 0, y: 0, rotation: 0}
-        this.velocity = {x: 0, y: 0, rotation: 0}
-        this.acceleration = {x: 0, y: 0, rotation: 0}
+        this.position.reset();
+        this.velocity.reset();
+        this.acceleration.reset();
         this.drag = 0;
     }
 }
