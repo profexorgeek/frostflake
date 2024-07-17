a0c6c3a31df2e05db613e3bcf5e27f0ab9825652
Justin Johnson <justin.d.johnson@gmail.com>
2024-07-17 11:19:09 -0600

    Grid now supports increment and axis highlighting

--- src/Positionables/Grid.ts
+++ src/Positionables/Grid.ts
@@ -5,8 +5,15 @@ import Positionable from "./Positionable";
 
 export default class Grid extends Positionable {
 
-    private _gridCount: number      = 0;
-    private _pixelSpacing: number   = 16;
+    private _gridCount: number = 0;
+    private _pixelSpacing: number = 16;
+    private _highlightIncrements: boolean = true;
+    private _highlightSpacing:number = 4;
+
+    private _xAxisColor: string = "red";
+    private _yAxisColor: string = "green";
+    private _alphaMultiplier: number = 0.5;
+
 
     override get alpha() {
         return this._alpha;
@@ -27,19 +34,42 @@ export default class Grid extends Positionable {
     get pixelSpacing(): number {
         return this._pixelSpacing;
     }
-
     set pixelSpacing(ps: number) {
         this._pixelSpacing = ps;
         this.calculateChildren();
     }
 
-    constructor(gridCount: number, pixelSpacing: number, color: string = "white") {
+    get highlightIncrements(): boolean {
+        return this._highlightIncrements;
+    }
+    set highlightIncrements(highlight: boolean) {
+        this._highlightIncrements = highlight;
+        this.calculateChildren();
+    }
+
+    get highlightSpacing(): number {
+        return this._highlightSpacing;
+    }
+    set highlightSpacing(spacing: number) {
+        this._highlightSpacing = Math.abs(spacing);
+        this.calculateChildren();
+    }
+
+    constructor(gridCount: number, pixelSpacing: number, color: string = "gray") {
         super();
 
         this._gridCount = gridCount;
         this._pixelSpacing = pixelSpacing;
         this._color = color;
         this.calculateChildren();
+    }
+
+    setLineColors(gridColor:string, xColor: string, yColor: string, alphaMultiplier: number)
+    {
+        this.color = gridColor;
+        this._xAxisColor = xColor;
+        this._yAxisColor = yColor;
+        this._alphaMultiplier = alphaMultiplier;
     }
 
     calculateChildren(): void {
@@ -51,16 +81,29 @@ export default class Grid extends Positionable {
         const lineLength = this._gridCount * this._pixelSpacing;
         for(let i = 0; i < this._gridCount + 1; i++)
         {
+            let targetAlpha = this._highlightIncrements === false || i % this._highlightSpacing == 0 ?
+                this.alpha :
+                this.alpha * this._alphaMultiplier;
+
             let horizontalLine = new Line(lineLength);
             horizontalLine.position.y = startY + (i * this._pixelSpacing);
             horizontalLine.color = this.color;
-            horizontalLine.alpha = this.alpha;
 
             let verticalLine = new Line(lineLength);
             verticalLine.position.x = startX + (i * this._pixelSpacing);
             verticalLine.rotation = MathUtil.toRadians(90);
             verticalLine.color = this.color;
-            verticalLine.alpha = this.alpha;
+
+            // override color and target alpha for x/y axis center lines
+            if(i == Math.floor(this._gridCount / 2))
+            {
+                verticalLine.color = this._yAxisColor;
+                horizontalLine.color = this._xAxisColor;
+                targetAlpha = this.alpha;
+            }           
+
+            horizontalLine.alpha = targetAlpha;
+            verticalLine.alpha = targetAlpha;
 
             this.addChild(horizontalLine);
             this.addChild(verticalLine);
